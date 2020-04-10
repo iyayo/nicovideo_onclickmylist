095eb12c806e3d46180962df45742cb05be12d65
ichijiku001 <toraneko2525ry@gmail.com>
2020-04-10 23:07:22 +0900

    変更：backgroundにマイリスト設定の読み込みを追加

--- js/background.js
+++ js/background.js
@@ -14,19 +14,34 @@ function getVideoData(videoId) {
   })
 }
 
+function getStorageMylist(){
+  return new Promise((resolve, reject) => {
+      chrome.storage.local.get('nvocm', function(value){
+          resolve(value.nvocm);
+      });
+  })
+}
+
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.message == "click") {
       const idRegex = /https:\/\/www\.nicovideo\.jp\/watch\/(..\d+)/;
       var url = sender.tab.url;
       var videoId = url.match(idRegex)[1];
-      getVideoData(videoId).done(function (value) {
-        const tokenRegex = /NicoAPI\.token = '(.*)';/;
-        var item_type = $(value).find('[name="item_type"]').val();
-        var item_id = $(value).find('[name="item_id"]').val();
-        var item_amc = $(value).find('[name="item_amc"]').val();
-        var token = value.match(tokenRegex)[1];
-      });
+
+      getStorageMylist().then(function(group_id){
+        if (group_id !== undefined){
+          getVideoData(videoId).done(function (value) {
+            const tokenRegex = /NicoAPI\.token = '(.*)';/;
+            var item_type = $(value).find('[name="item_type"]').val();
+            var item_id = $(value).find('[name="item_id"]').val();
+            var item_amc = $(value).find('[name="item_amc"]').val();
+            var token = value.match(tokenRegex)[1];
+          });
+        } else {
+          sendResponse({status: "登録先が設定されていません"})
+        }
+      })
     } else if (request.message == "mylist"){
       getMylist().done(function(value){
         var json = value.mylistgroup;
