90677e456e96072872ee05ed6c2c00dfe6520f1b
ichijiku001 <toraneko2525ry@gmail.com>
2020-04-10 22:56:11 +0900

    変更：マイリスト設定の保存＆復元機能の追加

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,34 @@
-function setMylist(json){
+function getStorageMylist(){
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get('nvocm', function(value){
+            resolve(value.nvocm);
+        });
+    })
+}
+
+function setStorageMylist(){
+    var id = $('select').val();
+    chrome.storage.local.set({nvocm: id}, function(){
+        $('p').text("保存しました");
+        $('p').fadeOut(3000);
+    });
+}
+
+function setMylist(json, restoreId){
     for (let i = 0; i < json.length; i++) {
-        $('select').append(`<option value=${json[i].id}>${json[i].name}</option>`);
+        if (restoreId !== undefined && restoreId == json[i].id){
+            $('select').append(`<option selected value=${json[i].id}>${json[i].name}</option>`);
+        } else {
+            $('select').append(`<option value=${json[i].id}>${json[i].name}</option>`);
+        }
     }
 }
 
 $(window).on('load',function(){
     chrome.runtime.sendMessage({ message: "mylist" }, function (response) {
-        setMylist(response.data);
+        getStorageMylist().then(function(value){
+            setMylist(response.data, value);
+        })
     });
+    $('#save').bind('click',setStorageMylist);
 });
