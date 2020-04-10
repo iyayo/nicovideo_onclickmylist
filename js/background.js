a22c2b52c64d5a3963a97395aaefd4c88494c7ec
ichijiku001 <toraneko2525ry@gmail.com>
2020-04-10 21:02:42 +0900

    変更：getMylistの追加＆これに伴いpermissionsの追加

--- js/background.js
+++ js/background.js
@@ -1,8 +1,16 @@
+function getMylist(){
+  return $.ajax({
+    url: 'http://www.nicovideo.jp/api/mylistgroup/list',
+    type: 'GET',
+    dataType: 'json'
+  })
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       var url = sender.tab.url;
-      if (request.message == "hello"){
-        sendResponse({farewell: "goodbye"});
+      if (request.message == "click"){
+        sendResponse({status: "goodbye"});
       }
 
       return true;
