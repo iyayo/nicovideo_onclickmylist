a1f9fe63af0b0f1129f00f5de70e56c504e673bc
ichijiku001 <toraneko2525ry@gmail.com>
2020-04-10 23:26:19 +0900

    変更：マイリスト登録の機能を追加

--- js/background.js
+++ js/background.js
@@ -14,6 +14,22 @@ function getVideoData(videoId) {
   })
 }
 
+function addMylist(group_id, item_type, item_id, item_amc, token){
+  return $.ajax({
+    url: 'http://www.nicovideo.jp/api/mylist/add',
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      "group_id": group_id,
+      "item_type": item_type,
+      "item_id": item_id,
+      "description": " ",
+      "item_amc": item_amc,
+      "token": token
+    }
+  })
+}
+
 function getStorageMylist(){
   return new Promise((resolve, reject) => {
       chrome.storage.local.get('nvocm', function(value){
@@ -29,21 +45,34 @@ chrome.runtime.onMessage.addListener(
       var url = sender.tab.url;
       var videoId = url.match(idRegex)[1];
 
-      getStorageMylist().then(function(group_id){
+      getStorageMylist()
+      .then(function(group_id){
         if (group_id !== undefined){
-          getVideoData(videoId).done(function (value) {
+          getVideoData(videoId)
+          .done(function (value) {
             const tokenRegex = /NicoAPI\.token = '(.*)';/;
             var item_type = $(value).find('[name="item_type"]').val();
             var item_id = $(value).find('[name="item_id"]').val();
             var item_amc = $(value).find('[name="item_amc"]').val();
             var token = value.match(tokenRegex)[1];
+
+            addMylist(group_id, item_type, item_id, item_amc, token)
+            .done(function(value){
+              console.log(value);
+              if (value.status == "ok") {
+                sendResponse({status: "マイリストに登録しました"});
+              } else {
+                sendResponse({status: value.error.description});
+              }
+            });
           });
         } else {
           sendResponse({status: "登録先が設定されていません"})
         }
       })
     } else if (request.message == "mylist"){
-      getMylist().done(function(value){
+      getMylist()
+      .done(function(value){
         var json = value.mylistgroup;
         sendResponse({data: json});
       });
