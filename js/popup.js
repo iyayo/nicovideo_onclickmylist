7d38fee8efbe1aab3aaa596015e0fcb69150630a
ichijiku001 <toraneko2525ry@gmail.com>
2020-04-24 14:05:41 +0900

    変更：オプションの追加（autoClose, selectSize）

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,11 @@
 function getStorageMylist(){
     return new Promise((resolve, reject) => {
-        chrome.storage.local.get(['nvocm_id', 'nvocm_desc'], (value) => {
+        chrome.storage.local.get([
+            'nvocm_id',
+            'nvocm_desc',
+            'nvocm_autoClose',
+            'nvocm_selectSize'
+        ], (value) => {
             resolve(value);
         });
     })
@@ -15,16 +20,21 @@ function setStatus(action){
     }
 }
 
-function setStorageMylist(){
-    var id = $('select').val();
-    var desc = $('textarea').val();
-    chrome.storage.local.set({nvocm_id: id}, () => {
+function setStorageMylist(autoClose){
+    let id = $('select').val();
+    let desc = $('textarea').val();
+    chrome.storage.local.set({
+        nvocm_id: id,
+        nvocm_desc: desc
+    }, () => {
         var index = $('select').prop('selectedIndex');
         chrome.browserAction.setBadgeText({'text': String(index + 1)}, () => {});
         setStatus("save");
-    });
 
-    chrome.storage.local.set({nvocm_desc: desc}, () => {});
+        if (autoClose == true){
+            window.close();
+        }
+    });
 }
 
 function setMylist(json, restoreId, restoreDesc){
@@ -53,13 +63,37 @@ function changeTab(link){
     });
 }
 
+function setStorageOption(){
+    chrome.storage.local.set({
+        nvocm_autoClose: $('#autoClose').prop('checked'),
+        nvocm_selectSize: $('#selectSize').val()
+    }, () => {
+        $('select').attr('size', $('#selectSize').val());
+    });
+}
+
+function restoreOption(){
+    getStorageMylist().then((value) => {
+        $('#autoClose').prop('checked', value.nvocm_autoClose);
+        if (value.nvocm_selectSize != undefined){
+            $('#selectSize').val(value.nvocm_selectSize);
+            $('select').attr('size', value.nvocm_selectSize);
+        }
+    });
+}
+
 $(window).on('load',function(){
+    restoreOption();
     chrome.runtime.sendMessage({ message: "mylist" }, (response) => {
         getStorageMylist().then((value) => {
             setMylist(response.data, value.nvocm_id, value.nvocm_desc);
-        })
+        });
     });
-    $('#save').bind('click',setStorageMylist);
+    $('#save').bind('click', () => {
+        getStorageMylist().then((value) => {
+            setStorageMylist(value.nvocm_autoClose);
+        });
+    });
     $('select').bind('change', () => {
         setStatus("select");
     });
@@ -67,4 +101,5 @@ $(window).on('load',function(){
         let link = $(this).attr('href');
         changeTab(link);
     });
+    $('.option').bind('change',setStorageOption);
 });
