5a547f941df3b6776a3383675d9c36fe93641b44
iyayo <toraneko2525ry@gmail.com>
2023-02-01 18:08:12 +0900

    [update] 動画をマイリストから削除&登録状態を確認できる機能を追加

--- js/background.js
+++ js/background.js
@@ -1,6 +1,7 @@
 const regexVideoId = /https:\/\/www\.nicovideo\.jp\/watch\/(..\d+)/;
 let notificationSound = false;
 let clearNotificationsTime = false;
+let match_itemId = "";
 
 chrome.storage.local.get(["nvocm_notificationSound", "nvocm_clearNotificationsTime"], item => {
     if (item.nvocm_notificationSound !== undefined) notificationSound = item.nvocm_notificationSound;
@@ -48,10 +49,7 @@ chrome.contextMenus.onClicked.addListener((info) => {
     Promise.resolve()
     .then(() => checkUserSession())
     .then(() => getStorageFormData())
-    .then(item => {
-        if (item[0] === "watchlater") return addWatchLater(videoId, encodeURIComponent(item[1]));
-        else return addMylist(item[0], videoId, encodeURIComponent(item[1]));
-    })
+    .then(item => addMylist(item[0], videoId, encodeURIComponent(item[1])))
     .then(res => createNotification(res, notificationSound, clearNotificationsTime))
     .catch(error => createNotification(error, notificationSound, clearNotificationsTime))
 });
@@ -63,10 +61,27 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         Promise.resolve()
         .then(() => checkUserSession())
         .then(() => getStorageFormData())
-        .then(item => {
-            if (item[0] === "watchlater") return addWatchLater(videoId, encodeURIComponent(item[1]));
-            else  return addMylist(item[0], videoId, encodeURIComponent(item[1]));
+        .then(item => addMylist(item[0], videoId, encodeURIComponent(item[1])))
+        .then(res => sendResponse(res))
+        .catch(error => sendResponse(error))
+    } else if (request.message === "checkMylist") {
+        const videoId = sender.tab.url.match(regexVideoId)[1];
+
+        Promise.resolve()
+        .then(() => checkUserSession())
+        .then(() => getStorageFormData())
+        .then(item => getMylistInfo(item[0], videoId))
+        .then(res => {
+            if (res) sendResponse(true);
+            else sendResponse(false);
         })
+    }
+
+    else if (request.message === "deleteMylist") {
+        Promise.resolve()
+        .then(() => checkUserSession())
+        .then(() => getStorageFormData())
+        .then(item => deleteMylist(item[0], match_itemId))
         .then(res => sendResponse(res))
         .catch(error => sendResponse(error))
     }
@@ -96,40 +111,124 @@ function getStorageFormData() {
     })
 }
 
-function addWatchLater(videoId, description) {
+function addMylist(mylistId, videoId, description) {
     return new Promise ((resolve, reject) => {
-        fetch(`https://nvapi.nicovideo.jp/v1/users/me/watch-later?watchId=${videoId}&memo=${description}`, {
+        if (mylistId == "watchlater") {
+            fetch(`https://nvapi.nicovideo.jp/v1/users/me/watch-later?watchId=${videoId}&memo=${description}`, {
             "headers": {
                 "x-frontend-id": "23",
                 "x-request-with": "N-garage"
             },
             "method": "POST",
             "credentials": "include"
-        })
-        .then(res => {
-            if (res.status === 201) resolve("あとで見るに登録しました");
-            else if (res.status === 409) resolve("あとで見るに登録済みです");
-            else resolve("登録に失敗しました");
-        })
+            })
+            .then(res => {
+                if (res.status === 201) resolve("あとで見るに登録しました");
+                else if (res.status === 409) resolve("あとで見るに登録済みです");
+                else resolve("登録に失敗しました");
+            })
+        } else {
+            fetch(`https://nvapi.nicovideo.jp/v1/users/me/mylists/${mylistId}/items?itemId=${videoId}&description=${description}`, {
+                "headers": {
+                    "x-frontend-id": "23",
+                    "x-request-with": "N-garage"
+                },
+                "method": "POST",
+                "credentials": "include"
+            })
+            .then(res => {
+                if (res.status === 201) resolve("マイリストに登録しました");
+                else if (res.status === 200) resolve("マイリストに登録済みです");
+                else resolve("登録に失敗しました");
+            })
+        }
     })
 }
 
-function addMylist(mylistId, videoId, description) {
+function deleteMylist(mylistId, itemId) {
     return new Promise ((resolve, reject) => {
-        fetch(`https://nvapi.nicovideo.jp/v1/users/me/mylists/${mylistId}/items?itemId=${videoId}&description=${description}`, {
-            "headers": {
-                "x-frontend-id": "23",
-                "x-request-with": "N-garage"
-            },
-            "method": "POST",
-            "credentials": "include"
-        })
-        .then(res => {
-            if (res.status === 201) resolve("マイリストに登録しました");
-            else if (res.status === 200) resolve("マイリストに登録済みです");
-            else resolve("登録に失敗しました");
-        })
+        if (mylistId == "watchlater") {
+            fetch(`https://nvapi.nicovideo.jp/v1/users/me/watch-later?itemIds=${itemId}`, {
+                "headers": {
+                    "x-frontend-id": "23",
+                    "x-request-with": "N-garage"
+                },
+                "method": "DELETE",
+                "credentials": "include"
+            })
+            .then(res => {
+                if (res.status === 200) resolve("マイリストから削除しました");
+                else resolve("削除に失敗しました");
+            })
+        } else {
+            fetch(`https://nvapi.nicovideo.jp/v1/users/me/mylists/${mylistId}/items?itemIds=${itemId}`, {
+                "headers": {
+                    "x-frontend-id": "23",
+                    "x-request-with": "N-garage"
+                },
+                "method": "DELETE",
+                "credentials": "include"
+            })
+            .then(res => {
+                if (res.status === 200) resolve("マイリストから削除しました");
+                else resolve("削除に失敗しました");
+            })
+        }
     })
+}
+
+async function getMylistInfo(id, videoId) {
+    let hasNext = true;
+    let match = false;
+    match_itemId = "";
+
+    for (let page = 1; hasNext && !match && page < 6; page++){
+        await myFunc(page, id, videoId);
+    }
+
+    return match;
+
+    async function myFunc(page, id, videoId) {
+        return new Promise((resolve) => {
+            if (id === "watchlater") {
+                fetch(`https://nvapi.nicovideo.jp/v1/users/me/watch-later?sortKey=addedAt&sortOrder=desc&pageSize=100&page=${page}`, { "headers": { "x-frontend-id": "6" }, "method": "GET" })
+                .then(response => response.json())
+                .then(obj => obj.data.watchLater)
+                .then(mylist => {
+                    hasNext = mylist.hasNext;
+    
+                    for (let i = 0; i < mylist.items.length; i++) {
+                        const item = mylist.items[i];
+    
+                        if (item.watchId == videoId) {
+                            match = true;
+                            match_itemId = item.itemId;
+                        } 
+                    }
+    
+                    resolve();
+                })
+            } else {
+                fetch("https://nvapi.nicovideo.jp/v1/users/me/mylists/" + id + `?page=${page}&pageSize=100`, { "headers": { "x-frontend-id": "6" }, "method": "GET" })
+                .then(response => response.json())
+                .then(obj => obj.data.mylist)
+                .then(mylist => {
+                    hasNext = mylist.hasNext;
+    
+                    for (let i = 0; i < mylist.items.length; i++) {
+                        const item = mylist.items[i];
+    
+                        if (item.watchId == videoId) {
+                            match = true;
+                            match_itemId = item.itemId;
+                        } 
+                    }
+    
+                    resolve();
+                })
+            }
+        });
+    }
 }
 
 function createNotification(res, silent, clear) {
--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 window.onload = function () {
+    let checkMylist_status = false;
+    const observer = new MutationObserver(checkMylist);
+    const target = document.querySelector("title");
+    const config = { childList: true };
+
+    observer.observe(target, config);
+
     let name = "ワンクリックマイリスト";
 
     const parentNode = document.querySelector(".VideoMenuContainer-areaLeft");
@@ -10,8 +17,8 @@ window.onload = function () {
     button.id = "onclick_mylist";
     button.className = "ActionButton WatchLaterButton VideoMenuContainer-button";
     button.dataset.title = name;
-    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>`;
-
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"/></svg>`;
+    
     parentNode.insertBefore(div, referenceNode);
     div.appendChild(button);
 
@@ -19,6 +26,8 @@ window.onload = function () {
         if (item.nvocm_name !== undefined) {
             name = item.nvocm_name;
             button.dataset.title = item.nvocm_name;
+
+            checkMylist();
         }
     })
 
@@ -26,6 +35,8 @@ window.onload = function () {
         if (item.nvocm_name) {
             name = item.nvocm_name.newValue;
             button.dataset.title = item.nvocm_name.newValue;
+
+            checkMylist();
         }
     })
 
@@ -37,12 +48,38 @@ window.onload = function () {
     }
 
     function addMylist() {
-        chrome.runtime.sendMessage({ message: "addMylist" }, (response) => {
-            if (response === "あとで見るに登録しました" || response === "マイリストに登録しました") button.classList.add("is-succeeded");
-            else button.classList.add("is-failed");
+        if (!checkMylist_status) {
+            chrome.runtime.sendMessage({ message: "addMylist" }, (response) => {
+                if (response === "あとで見るに登録しました" || response === "マイリストに登録しました") button.classList.add("is-succeeded");
+                else button.classList.add("is-failed");
+    
+                checkMylist();
+                button.dataset.title = response;
+                timeoutID = window.setTimeout(resetDataTitle, 5000);
+            });
+        } else {
+            chrome.runtime.sendMessage({ message: "deleteMylist" }, (response) => {
+                if (response === "あとで見るに登録しました" || response === "マイリストから削除しました") button.classList.add("is-succeeded");
+                else button.classList.add("is-failed");
+    
+                checkMylist();
+                button.dataset.title = response;
+                timeoutID = window.setTimeout(resetDataTitle, 5000);
+            });
+        }
+    }
 
-            button.dataset.title = response;
-            timeoutID = window.setTimeout(resetDataTitle, 5000);
+    function checkMylist() {
+        chrome.runtime.sendMessage({ message: "checkMylist" }, (response) => {
+            if (response) {
+                button.style.fill = "#007cff";
+                button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"/></svg>`;
+            } else {
+                button.style.fill = null;
+                button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z"/></svg>`;
+            }
+
+            checkMylist_status = response;
         });
     }
 }
