a87bd0e69fce400a53dc93f0a15124ce45a1f399
iyayo <toraneko2525ry@gmail.com>
2024-04-23 19:00:01 +0900

    [fix] 一定時間経過するとマイリスト追加時にエラーが発生する問題を修正

--- js/background.js
+++ js/background.js
@@ -250,3 +250,7 @@ function createNotification(res, silent, clear) {
         chrome.alarms.create(notificationId, {"delayInMinutes": 1})
     });
 }
+
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20e3);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
